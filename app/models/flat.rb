f43047b0e50e5d7ce2c719510e24b8f409edf7a8
Nina Karas <KarasNina@googlemail.com>
2019-05-21 16:22:05 +0200

    cards styling & home view

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,23 @@
 class Flat < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  def all_reviews_for_flat
+    self.bookings.map { |booking| booking.review }
+  end
+
+  def average_rating
+    sum = 0
+    reviews = all_reviews_for_flat
+    return 0 if reviews == []
+    reviews.each do |review|
+      sum += review.stars unless review.nil?
+    end
+    sum / reviews.size
+  end
+
+  def sum_rating
+    reviews = all_reviews_for_flat
+    reviews.count
+  end
 end
