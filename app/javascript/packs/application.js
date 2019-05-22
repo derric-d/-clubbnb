2c386e40903b01ddc6656080e807c9a03cb9638b
Nina Karas <KarasNina@googlemail.com>
2019-05-22 12:55:59 +0200

    gecocoding and map added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,13 @@
+
 import "bootstrap";
-import '../components/_navbar.js'
+import '../components/_navbar.js';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+
+import { initMapbox } from '../plugins/init_mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
+
+initMapbox();
+initAutocomplete();
