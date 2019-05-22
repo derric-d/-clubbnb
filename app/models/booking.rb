3185313c88f5a684b7459d360ba5281cd66d8b2a
Nina Karas <KarasNina@googlemail.com>
2019-05-22 16:50:20 +0200

    profile page started

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,5 +4,11 @@ class Booking < ApplicationRecord
 
   has_one :review
 
+
+  # def all_bookings_for_user
+  #   self.bookings.map do |booking|
+  #   booking.review
+  #   end
+  # end
   # validates :status, inclusion: { in: ['requested by guest', 'confirmed by host', 'cancelled by guest', 'denied by host', 'paid']}
 end
