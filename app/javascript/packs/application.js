08015b2cffe170b12632a06897ae5fc7a44609c2
Stephan Grund <stephan.grund@whu.edu>
2019-05-23 17:28:13 +0200

    booking view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,4 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initAutocomplete } from '../plugins/init_autocomplete';
-
-
-initMapbox();
-initAutocomplete();
+import 'plugins/flatpickr';
